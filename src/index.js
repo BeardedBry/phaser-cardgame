3ebe8ab888df100c358aeb0372c4182b00e0d7c9
Burkard <burkarbr@laptop-burkarbr.home>
2020-05-14 23:25:37 -0400

    following tut

--- src/index.js
+++ src/index.js
@@ -1,32 +1,29 @@
 import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+import Game from "./scenes/game";
 
 const config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create
-  }
+  width: 1280,
+  height: 780,
+  scene: [Game]
 };
 
 const game = new Phaser.Game(config);
 
-function preload() {
-  this.load.image("logo", logoImg);
-}
+// function preload() {
+//   this.load.image("logo", logoImg);
+// }
 
-function create() {
-  const logo = this.add.image(400, 150, "logo");
+// function create() {
+//   const logo = this.add.image(400, 150, "logo");
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
-}
+//   this.tweens.add({
+//     targets: logo,
+//     y: 450,
+//     duration: 2000,
+//     ease: "Power2",
+//     yoyo: true,
+//     loop: -1
+//   });
+// }
