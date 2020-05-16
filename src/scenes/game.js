27d5b16b294efaa1bb0ce0a9ee72794f24b16c1e
Burkard <burkarbr@laptop-burkarbr.home>
2020-05-15 23:28:47 -0400

    dropzone

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,4 +1,5 @@
 import Card from "../helpers/card";
+import Zone from "../helpers/zone";
 
 export default class Game extends Phaser.Scene {
   constructor() {
@@ -47,6 +48,31 @@ export default class Game extends Phaser.Scene {
       gameObject.x = dragX;
       gameObject.y = dragY;
     });
+
+    this.input.on("dragstart", function(pointer, gameObject) {
+      gameObject.setTint(0xff69b4);
+      self.children.bringToTop(gameObject);
+    });
+
+    this.input.on("dragend", function(pointer, gameObject, dropped) {
+      gameObject.setTint();
+      if (!dropped) {
+        gameObject.x = gameObject.input.dragStartX;
+        gameObject.y = gameObject.input.dragStartY;
+      }
+    });
+
+    this.input.on("drop", function(pointer, gameObject, dropZone) {
+      console.log(dropZone);
+      dropZone.data.values.cards++;
+      gameObject.x = dropZone.x - 350 + dropZone.data.values.cards * 50;
+      gameObject.y = dropZone.y;
+      gameObject.disableInteractive();
+    });
+
+    this.zone = new Zone(this);
+    this.dropZone = this.zone.renderZone();
+    this.outline = this.zone.renderOutline(this.dropZone);
   }
 
   update() {}
